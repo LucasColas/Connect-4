77030ae0cc8e126b8efc16ce1b95e6cdbef1cd51
lucascolas <lucas.colas13@gmail.com>
2024-04-28 17:40:23 +0200

    add argparse

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ Game object and calls the play method to start the game.
 
 """
 
-import pygame
 from Game.game import Game
+import argparse
 
-def main():
-    # to do : argparse the number of cols and rows and why not the size of the window
-    Connect4 = Game()
-    Connect4.play()
+
+
+
+    
 
 if __name__ == "__main__":
-    main()
+    # argparse the number of cols and rows
+    parser = argparse.ArgumentParser(description="Connect 4 game")
+    parser.add_argument("--rows", type=int, default=6, help="Number of rows in the grid")
+    parser.add_argument("--cols", type=int, default=7, help="Number of cols in the grid")
+    args = parser.parse_args()
+    Connect4 = Game(args.rows, args.cols)
+    Connect4.play()
