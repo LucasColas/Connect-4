d7d96496d705fd065b3dce7c481be5dc1a58df71
lucascolas <lucas.colas13@gmail.com>
2024-04-29 16:52:55 +0200

    comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 Entry point of the game. It creates a 
 Game object and calls the play method to start the game.
+You can also pass the number of rows and columns as arguments.
 
 """
 
@@ -9,12 +10,10 @@ from Game.game import Game
 import argparse
 
 
-
-
     
 
 if __name__ == "__main__":
-    # argparse the number of cols and rows
+    
     parser = argparse.ArgumentParser(description="Connect 4 game")
     parser.add_argument("--rows", type=int, default=6, help="Number of rows in the grid")
     parser.add_argument("--cols", type=int, default=7, help="Number of cols in the grid")
